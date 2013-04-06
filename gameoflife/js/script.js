7fa3cbca8217c5fd0efa85c4ebb6d185b1585c89
David Ignacio <deignacio@gmail.com>
2013-04-06 01:16:44 -0700

    update js simulation to loop lineages as well as generations

--- gameoflife/js/script.js
+++ gameoflife/js/script.js
@@ -1,32 +1,67 @@
-function oneLineage(size) {
-    console.log("running a lineage, world size: " + size);
-    var gol = new GameOfLife({"size": size});
+function dumpStats(stats) {
+    if (this.genCount === undefined) {
+        this.genCount = 0;
+    }
+    this.genCount += stats.genCount;
+    console.log("stopped after " + stats.genCount + " generations, " + this.genCount + "  generations total");
+}
+
+function dumpWorld(cells) {
+    console.log(cells.join(", "));
+}
+
+function init() {
+    var runLineage = true;
+    var gol = null;
     var cells = [];
-    var stats = {"genCount": 0, "cellCount": cells.length};
-    var keepRunning = true;
-    var oneGeneration = function() {
-        gol.nextGeneration();
-        gol.dumpGeneration(function(val, x, y) {
-            cells.push("(" + x + ", " + y + ")");
-        });
-        stats.genCount++;
-        stats.cellCount = cells.length;
-        console.log(stats.cellCount);
-        if (cells.length > 0 && keepRunning) {
-            cells = [];
-            setTimeout(oneGeneration, 0);
-        } else {
-            dumpStats(stats);
-            console.log("stopped after " + stats.genCount + " generations");
+    var stats = {};
+    var killLineageId = -1;
+    var generationId = -1;
+    var oneLineage = function(size) {
+        killLineageId = setTimeout(function() {
+            console.log("lineage short circuited");
+            if (generationId != -1) {
+                clearTimeout(generationId);
+            }
+            if (runLineage) {
+                dumpStats(stats);
+                oneLineage(size);
+            }
+        }, 2500);
+        gol = new GameOfLife({"size": size});
+        cells = [];
+        stats = {"genCount": 0, "cellCount": cells.length};
+        var oneGeneration = function() {
+            gol.nextGeneration();
+            gol.dumpGeneration(function(val, x, y) {
+                cells.push("(" + x + ", " + y + ")");
+            });
+            dumpWorld(cells);
+            stats.genCount++;
+            stats.cellCount = cells.length;
+            if (runLineage) {
+                if (cells.length > 0) {
+                    cells = [];
+                    generationId = setTimeout(oneGeneration, 0);
+                } else {
+                    if (killLineageId != -1) {
+                        clearTimeout(killLineageId);
+                    }
+                    dumpStats(stats);
+                    oneLineage(size);
+                }
+            } else {
+                console.log("simulation short circuited");
+                dumpStats(stats);
+            }
         }
+        oneGeneration();
     }
     setTimeout(function() {
-        keepRunning = false;
+        runLineage = false;
         console.log("stopping");
-    }, 5000);
-    oneGeneration();
-}
-function init() {
+    }, 10000);
+
     oneLineage("small");
 }
 
