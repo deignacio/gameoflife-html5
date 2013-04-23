6b676a8fa7c4b3eac9c02991c3c88578537a5710
David Ignacio <deignacio@gmail.com>
2013-04-23 15:31:19 +0000

    simplify world logic, combine lineage and generation runtimes

--- gameoflife/js/world.js
+++ gameoflife/js/world.js
@@ -6,53 +6,27 @@ define(function(require) {
         } else {
             this.running = true;
         }
-        var runLineage = true;
-        var gol = null;
-        var stats = {};
-        var renderer = null;
-        var killLineageId = -1;
         var generationId = -1;
-        var minPopulation = -1;
-        var maxLineageRuntime = 10000;
-        var maxWorldRuntime = 30000;
-        var oneLineage = function() {
-            killLineageId = setTimeout(function() {
-                if (generationId != -1) {
-                    clearTimeout(generationId);
+        var populationThreshold = 0.01;
+        var maxWorldRuntime = 300000;
+        var generationDelay = 50;
+        var gol = new GameOfLife({"size": worldSize});
+        var renderer = new RendererFactory({world: gol, divId: "gol"});
+        var minPopulation = gol.cellCount * populationThreshold;
+        var oneGeneration = function() {
+            gol.nextGeneration();
+            renderer.render();
+            if (this.running) {
+                if (gol.populationSize < minPopulation) {
+                    gol.initialize();
                 }
-                if (runLineage) {
-                    renderer.clear();
-                    oneLineage();
-                }
-            }, maxLineageRuntime);
-            gol = new GameOfLife({"size": worldSize});
-            renderer = new RendererFactory({world: gol, divId: "gol"});
-            stats = {"genCount": 0, "cellCount": gol.populationSize};
-            minPopulation = gol.cellCount * 0.01;
-            var oneGeneration = function() {
-                gol.nextGeneration();
-                renderer.render();
-                stats.genCount++;
-                stats.cellCount = gol.populationSize;
-                if (runLineage) {
-                    if (gol.populationSize > minPopulation) {
-                        generationId = setTimeout(oneGeneration, 50);
-                    } else {
-                        if (killLineageId != -1) {
-                            clearTimeout(killLineageId);
-                        }
-                        renderer.clear();
-                        oneLineage();
-                    }
-                }
+                generationId = setTimeout(oneGeneration, generationDelay);
             }
-            oneGeneration();
         }
         setTimeout(function() {
-            runLineage = false;
             this.running = false;
         }, maxWorldRuntime);
-        oneLineage();
+        oneGeneration();
     }
     return startWorld;
 });
