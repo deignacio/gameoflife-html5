78427ffc24e2151c38dd46c59733f536f7b11ffd
David Ignacio <deignacio@gmail.com>
2013-04-24 00:30:16 -0700

    fix index.html

--- gameoflife/js/canvasrenderer.js
+++ gameoflife/js/canvasrenderer.js
@@ -5,13 +5,6 @@ define(function(require) {
     }
     CanvasWorldRenderer.prototype = new WorldRenderer();
     CanvasWorldRenderer.prototype._initialize = function() {
-        if (!this._parent) {
-            var canvas = document.createElement("canvas");
-            canvas.width = 640;
-            canvas.height = 640;
-            document.body.appendChild(canvas);
-            this._parent = canvas;
-        }
         this._cwidth = this._parent.width / this._gol.width;
         this._cheight = this._parent.height / this._gol.height;
         this._ctx = this._parent.getContext('2d');
--- gameoflife/js/main.js
+++ gameoflife/js/main.js
@@ -1,14 +1,17 @@
-requirejs(['helpers', 'canvasrenderer', 'world'],
-function (helpers, CanvasWorldRenderer, startWorld) {
+requirejs(['helpers', 'world'],
+function (helpers, startWorld) {
     console.log("regular init");
     function startWorldDom(data) {
         var dataset = helpers.bubbleUpToClass(data.target, "gol-start").dataset;
         var worldSize = (dataset && ("worldSize" in dataset)) ? dataset.worldSize : "large";
-        startWorld(worldSize, CanvasWorldRenderer);
+        var canvas = document.getElementById("gol");
+        startWorld(worldSize, canvas);
     }
 
-    var sizes = document.getElementsByClassName("gol-start");
-    for (var i = 0; i < sizes.length; i++) {
-        sizes[i].addEventListener("click", startWorldDom);
-    }
+    // var sizes = document.getElementsByClassName("gol-start");
+    // for (var i = 0; i < sizes.length; i++) {
+    //     sizes[i].addEventListener("click", startWorldDom);
+    // }
+    var canvas = document.getElementById("gol");
+    startWorld("large", canvas);
 });
